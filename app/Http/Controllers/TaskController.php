da37e5c2a9e5f67ffded05c58036e5ac43ec8259
Otabek <otabekrahmonovtest@gmail.com>
2024-04-03 11:06:36 +0500

    feat: Model resource

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,9 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
+use App\Http\Resources\TaskCollection;
+
 class TaskController extends Controller
 {
+    // public function index(Request $request){
+    //     return response()->json(Task::all());
+    // }
+
     public function index(Request $request){
-        return response()->json(Task::all());
+        // dd(Task::all());
+        return new TaskCollection(Task::all());
     }
 }

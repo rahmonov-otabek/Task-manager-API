7d8049bf6c31de5f048b2e8672052720b16410c9
Otabek <otabekrahmonovtest@gmail.com>
2024-04-03 10:17:23 +0500

    feat: First route

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(Request $request){
+        return response()->json(Task::all());
+    }
+}

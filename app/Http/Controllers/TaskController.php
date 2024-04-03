ddbef57633d5d29667e41f878c1f00508648e9eb
Otabek <otabekrahmonovtest@gmail.com>
2024-04-03 15:40:42 +0500

    feat: Sorting

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -19,6 +19,8 @@ class TaskController extends Controller
     {
         $tasks = QueryBuilder::for(Task::class)
             ->allowedFilters('is_done')
+            ->defaultSort('-created_at')
+            ->allowedSorts('title', 'is_done', 'created_at')
             ->paginate();
 
         return new TaskCollection($tasks);

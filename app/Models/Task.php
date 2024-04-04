cc903b69e46a0ea81278ed8c6687bc87ea27e62e
Otabek <otabekrahmonovtest@gmail.com>
2024-04-04 17:13:38 +0500

    feat: Tasks That Belong to Users

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -17,8 +17,8 @@ class Task extends Model
     protected $casts = [
         'is_done' => 'boolean',
     ];
-
-    protected $hidden = [
-        'updated_at',
-    ];
+ 
+    public function creator(){
+        return $this->belongsTo(User::class, 'creator_id');
+    }
 }

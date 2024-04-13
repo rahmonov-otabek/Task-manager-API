790ef6f75015ab742e2f85d1fb8a2a9b691a9a23
Otabek <otabekrahmonovtest@gmail.com>
2024-04-13 23:52:02 +0500

    Filter List Responses by Membership

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -13,22 +13,15 @@ class ProjectPolicy
      */
     public function viewAny(User $user): bool
     {
-        //
+        return true;
     }
 
     /**
      * Determine whether the user can view the model.
      */
     public function view(User $user, Project $project): bool
-    {
-        if ($user->id === $project->creator_id){
-            return true;
-        }
-        if ($user->memberships->contains($project)){
-            return true;
-        }
-
-        return false;
+    { 
+        return $user->memberships->contains($project); 
     }
 
     /**
